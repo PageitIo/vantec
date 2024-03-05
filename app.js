108d0ef8ced61b214129c7855b41eda288f73361
Romeo <romep@pageit.io>
2024-03-05 20:16:21 +0000

    wdwdwdewd

--- app.js
+++ app.js
@@ -3,3 +3,20 @@
 
 /* Login arrow animation */
 
+document.addEventListener('DOMContentLoaded', function() {
+    const directionWrappers = document.querySelectorAll('.direction-wrapper');
+    let currentIndex = directionWrappers.length - 1;
+    function showNextDirectionWrapper() {
+    
+        directionWrappers.forEach(wrapper => {
+            wrapper.style.display = 'none';
+        });
+    
+        const currentWrapper = directionWrappers[currentIndex];
+        currentWrapper.style.display = 'block';
+    
+        currentIndex = (currentIndex - 1 + directionWrappers.length) % directionWrappers.length;
+    }
+    showNextDirectionWrapper();
+    setInterval(showNextDirectionWrapper, 1000);
+});
